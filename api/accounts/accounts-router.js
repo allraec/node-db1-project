19f5da0811de68928ffad091d329cdfb813587b1
Allison Castaneda <allraec@yahoo.com>
2021-04-21 17:17:36 -0500

    getAll and get / done

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,7 +1,13 @@
+const db = require("./accounts-model")
 const router = require('express').Router()
 
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/', async (req, res, next) => {
+  try{
+    const accounts = await db.getAll()
+    res.json(accounts);
+  }catch (err) {
+    next(err)
+  }
 })
 
 router.get('/:id', (req, res, next) => {
